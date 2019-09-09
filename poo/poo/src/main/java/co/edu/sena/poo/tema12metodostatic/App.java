8dcc17282d2595254116833fc52bb63adbdd52f3
Hernando Enrique Moreno Moreno <hemoreno33@misena.edu.co>
2019-09-09 13:22:01 -0500

    chore: poo ejemplos de metodos statics

---
+++ poo/poo/src/main/java/co/edu/sena/poo/tema12metodostatic/App.java
@@ -0,0 +1,7 @@
+package co.edu.sena.poo.tema12metodostatic;
+
+public class App {
+    public static void main(String[] args) {
+        Matematica.sumar(6,8);
+    }
+}
---
+++ poo/poo/src/main/java/co/edu/sena/poo/tema12metodostatic/Matematica.java
@@ -0,0 +1,11 @@
+package co.edu.sena.poo.tema12metodostatic;
+
+public class Matematica {
+    public static void sumar(int a, int b){
+        System.out.println(a+b);
+    }
+
+    public static long sumar(long a, long b){
+        return a+b;
+    }
+}
